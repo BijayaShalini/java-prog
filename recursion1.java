83d261ba0bf4437a2901fcf46ac293046daf1978
Bijaya Shalini <bijayashalini999@gmail.com>
2021-07-16 22:24:44 +0530

    Recursion example

---
+++ recursion1.java
@@ -0,0 +1,22 @@
+// recursion function
+import java.util.Scanner;
+
+class Factorial {
+    int fact(int n){
+        int result;
+        if(n==1)
+            return 1;
+        result=fact(n-1)*n;
+        return result;
+    }
+}
+class recursion1{
+    public static void main(String args[]){
+        Factorial f= new Factorial();
+        int n;
+        System.out.println("Enter number:");
+        Scanner sc= new Scanner(System.in);
+        n=sc.nextInt();
+        System.out.println("Factorial is:"+ f.fact(n));
+    }
+}
